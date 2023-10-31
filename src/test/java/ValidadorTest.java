d04bed5a3d46468e6bd85119614100f67dde41ed
rafper <rafperdevops@gmail.com>
2023-10-30 19:02:24 -0500

    Test Clase Validador

---
+++ src/test/java/ValidadorTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+
+import com.fedecafe.com.fedecafe.inventario.Validador;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author koto
+ */
+public class ValidadorTest {
+    @Test
+    public void testEsCodigoValido(){
+        //Prueba un código válido
+        assertTrue(Validador.esCodigoValido("123456"));
+        //Prueba un código inválido
+        assertFalse(Validador.esCodigoValido("sdfsdf"));
+    }
+
+}
